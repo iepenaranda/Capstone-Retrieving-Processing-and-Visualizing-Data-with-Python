e7b68e4d27ba4d1de4a8b1bd5c49ab0f4357e546
Iván <iepenaranda>
2021-01-08 19:48:01 -0500

    Agregar codigo para graficar usando JavaScript

---
+++ graficar.py
@@ -0,0 +1,41 @@
+import sqlite3
+import time
+import zlib
+
+conn = sqlite3.connect('covid.sqlite')
+cur = conn.cursor()
+
+cur.execute('SELECT fecha, total_cases, total_deaths FROM dias')
+total = dict()
+for filas in cur:
+    total[filas[0]] = (filas[1], filas[2])
+
+#print(cases.keys())
+
+dates = list()
+for date in total.keys():
+    dates.append(date)
+
+cur.execute('SELECT fecha, new_cases, new_deaths FROM dias')
+news = dict()
+for filas in cur:
+    news[filas[0]] = (filas[1], filas[2])
+
+print('Loaded days: ',len(dates))
+
+fhand = open('gline.js','w')
+fhand.write("gline = [ ['Date', 'New Cases','New Deaths']")
+
+for i in range(len(dates)):
+    fhand.write(",\n['"+dates[i]+"'")
+    fhand.write(","+str(news.get(dates[i])[0]))
+    print((total.get(dates[i])))
+
+    fhand.write(","+str(news.get(dates[i])[1]))
+    fhand.write("]")
+
+fhand.write("\n];\n")
+fhand.close()
+
+print("Output written to gline.js")
+print("Open gline.htm to visualize the data")
--- leer.py
+++ leer.py
@@ -6,7 +6,7 @@ con = sqlite3.connect('covid.sqlite')
 cur = con.cursor()
 cur.execute('''CREATE TABLE IF NOT EXISTS dias
     (id INTEGER PRIMARY KEY AUTOINCREMENT, iso_code TEXT, continent TEXT, 
-    location TEXT, dia INTEGER, mes INTEGER, año INTEGER, total_cases INTEGER, new_cases INTEGER, 
+    location TEXT, fecha TEXT, total_cases INTEGER, new_cases INTEGER, 
     total_deaths INTEGER, new_deaths INTEGER, total_tests INTEGER, new_tests INTEGER)''')
 
 with open('data\owid-covid-data.csv', newline='') as csvfile:
@@ -18,15 +18,27 @@ with open('data\owid-covid-data.csv', newline='') as csvfile:
             pass
         else:
             if row[2] == 'Colombia':
-                fecha = row[3]
-                date_time_obj = datetime.strptime(fecha, '%d/%m/%Y')
-                dia = date_time_obj.day
-                mes = date_time_obj.month
-                año = date_time_obj.year
+                #fecha = row[3]
+                #date_time_obj = datetime.strptime(fecha, '%d/%m/%Y')
+                #dia = date_time_obj.day
+                #mes = date_time_obj.month
+                #año = date_time_obj.year
 
+                if row[4] == "":
+                    row[4] = 0
+                if row[5] == "":
+                    row[5] = 0
+                if row[7] == "":
+                    row[7] = 0
+                if not row[8]:
+                    row[8] = 0
+                if row[25] == "":
+                    row[25] = 0
+                if row[26] is "":
+                    row[26] = 0
                 cur.execute('''INSERT OR IGNORE INTO dias (iso_code, continent, location,
-                dia, mes, año, total_cases, new_cases, total_deaths, new_deaths, new_tests, total_tests) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)''',
-                (row[0],row[1],row[2],dia,mes,año,row[4],row[5],row[7],row[8],row[25],row[26]))
+                fecha, total_cases, new_cases, total_deaths, new_deaths, new_tests, total_tests) VALUES (?,?,?,?,?,?,?,?,?,?)''',
+                (row[0],row[1],row[2],row[3],row[4],row[5],row[7],row[8],row[25],row[26]))
                 con.commit()
                 i += 1
                 print(i)
