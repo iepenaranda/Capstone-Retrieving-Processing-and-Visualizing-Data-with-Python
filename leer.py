f52d5a8b0034508a2588d9a547db34cf23052a41
Iván <iepenaranda>
2021-01-09 21:53:48 -0500

    Se agregó opción para escoger pais y una interrupción por teclado

--- leer.py
+++ leer.py
@@ -10,38 +10,38 @@ cur.execute('''CREATE TABLE IF NOT EXISTS dias
     total_deaths INTEGER, new_deaths INTEGER, total_tests INTEGER, new_tests INTEGER)''')
 
 with open('data\owid-covid-data.csv', newline='') as csvfile:
-    i = 0
-    lector = csv.reader(csvfile, delimiter=';')
-    for row in lector:
-        if i == 0:
-            i += 1
-            pass
-        else:
-            if row[2] == 'Colombia':
-                #fecha = row[3]
-                #date_time_obj = datetime.strptime(fecha, '%d/%m/%Y')
-                #dia = date_time_obj.day
-                #mes = date_time_obj.month
-                #año = date_time_obj.year
+    lector = csv.reader(csvfile, delimiter=',')
+    while True:
+        print('Enter the name of the country that you want to get data: ')
+        location = input()
+        print('Searching data of: '+location)
+        try:
+            for row in lector:
+                if row[2] == location:
+                    if row[4] == "":
+                        row[4] = 0
+                    if row[5] == "":
+                        row[5] = 0
+                    if not row[7]:
+                        row[7] = 0
+                    if not row[8]:
+                        row[8] = 0
+                    if not row[25]:
+                        row[25] = 0
+                    if not row[26]:
+                        row[26] = 0
+                    
+                    cur.execute('''INSERT OR IGNORE INTO dias (iso_code, continent, location,
+                    fecha, total_cases, new_cases, total_deaths, new_deaths, new_tests, total_tests)
+                    VALUES (?,?,?,?,?,?,?,?,?,?)''',
+                    (row[0],row[1],row[2],row[3],row[4],row[5],row[7],row[8],row[25],row[26]))
+                    con.commit()
+                else:
+                    pass
+        except KeyboardInterrupt:
+            print('')
+            print('Program interrupted by user...')
+            break
+cur.close()
 
-                if row[4] == "":
-                    row[4] = 0
-                if row[5] == "":
-                    row[5] = 0
-                if row[7] == "":
-                    row[7] = 0
-                if not row[8]:
-                    row[8] = 0
-                if row[25] == "":
-                    row[25] = 0
-                if row[26] is "":
-                    row[26] = 0
-                cur.execute('''INSERT OR IGNORE INTO dias (iso_code, continent, location,
-                fecha, total_cases, new_cases, total_deaths, new_deaths, new_tests, total_tests) VALUES (?,?,?,?,?,?,?,?,?,?)''',
-                (row[0],row[1],row[2],row[3],row[4],row[5],row[7],row[8],row[25],row[26]))
-                con.commit()
-                i += 1
-                print(i)
-            else:
-                pass            
-    cur.close()
+                
